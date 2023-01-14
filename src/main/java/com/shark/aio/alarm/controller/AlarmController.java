fcd102ecf32f433978010a926027981936836444
lbx“git config --global user.name lbx“ <836106309@qq.com>
2023-01-14 19:01:18 +0800

    调整格式

--- src/main/java/com/shark/aio/alarm/controller/AlarmController.java
+++ src/main/java/com/shark/aio/alarm/controller/AlarmController.java
@@ -16,63 +16,63 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/alarm")
 public class AlarmController {
-    @Autowired
-    private AlarmService alarmService;
+	@Autowired
+	private AlarmService alarmService;
 
-    @RequestMapping(value = {"/settings","/settings/{pageNum}/{pageSize}"})
-    public String toAlarmSettingsPage(HttpServletRequest request,
-                                      @PathVariable(required = false) Integer pageNum,
-                                      @PathVariable(required = false) Integer pageSize,
-                                      String feature){
-        PageInfo<AlarmEntity> alarmSettings = alarmService.getAlarmSettingsByPage(pageNum, pageSize, feature);
+	@RequestMapping(value = { "/settings", "/settings/{pageNum}/{pageSize}" })
+	public String toAlarmSettingsPage(HttpServletRequest request, @PathVariable(required = false) Integer pageNum,
+			@PathVariable(required = false) Integer pageSize, String feature) {
+		PageInfo<AlarmEntity> alarmSettings = alarmService.getAlarmSettingsByPage(pageNum, pageSize, feature);
 
-        request.setAttribute("allSettings",alarmSettings);
-        return Constants.ALARMSETTINGS;
-    }
+		request.setAttribute("allSettings", alarmSettings);
+		return Constants.ALARMSETTINGS;
+	}
 
-    @GetMapping("/settings/add")
-    public String toAddAlarmSettingPage(HttpServletRequest request){
-        if (!alarmService.setAttributeBYMonitorAndPollution(request)){
-            return "500";
-        }
-        return Constants.ADDALARMSETTING;
-    }
+	@GetMapping("/settings/add")
+	public String toAddAlarmSettingPage(HttpServletRequest request) {
+		if (!alarmService.setAttributeBYMonitorAndPollution(request)) {
+			return "500";
+		}
+		return Constants.ADDALARMSETTING;
+	}
 
+	@PostMapping("/settings/add")
+	public String addAlarmSetting(HttpServletRequest request, AlarmEntity alarmEntity, String newMonitorClass,
+			String newPollution, String existPollutionName, String existMonitorClass) {
+		String msg = alarmService.addAlarmSetting(alarmEntity, newMonitorClass, existMonitorClass, newPollution,
+				existPollutionName);
+		request.setAttribute(Constants.MSG, msg);
+		if (msg.contains("成功"))
+			return "forward:/alarm/settings";
+		else
+			return toAddAlarmSettingPage(request);
+	}
 
-    @PostMapping("/settings/add")
-    public String addAlarmSetting(HttpServletRequest request, AlarmEntity alarmEntity,
-                                  String newMonitorClass, String newPollution,
-                                  String existPollutionName, String existMonitorClass){
-        String msg = alarmService.addAlarmSetting(alarmEntity,newMonitorClass,existMonitorClass,newPollution,existPollutionName);
-        request.setAttribute(Constants.MSG, msg);
-        if (msg.contains("成功")) return "forward:/alarm/settings";
-        else return toAddAlarmSettingPage(request);
-    }
+	@GetMapping("/settings/delete/{id}")
+	public String deleteAlarmSetting(HttpServletRequest request, @PathVariable int id) {
+		String msg = alarmService.deleteAlarmSettingById(id);
+		request.setAttribute(Constants.MSG, msg);
+		return "forward:/alarm/settings";
+	}
 
+	@GetMapping("/settings/edit")
+	public String toEditAlarmSettingPage(HttpServletRequest request, AlarmEntity alarmEntity) {
+		request.setAttribute("alarmEntity", alarmEntity);
+		if (!alarmService.setAttributeBYMonitorAndPollution(request)) {
+			return "500";
+		}
+		return Constants.EDITALARMSETTING;
+	}
 
-    @GetMapping("/settings/delete/{id}")
-    public String deleteAlarmSetting(HttpServletRequest request, @PathVariable int id){
-        String msg = alarmService.deleteAlarmSettingById(id);
-        request.setAttribute(Constants.MSG, msg);
-        return "forward:/alarm/settings";
-    }
+	@PostMapping("/settings/edit")
+	public String editAlarmSetting(HttpServletRequest request, AlarmEntity alarmEntity) {
+		String msg = alarmService.editAlarmSetting(alarmEntity);
+		request.setAttribute(Constants.MSG, msg);
+		if (msg.contains("成功"))
+			return "forward:/alarm/settings";
+		else
+			return toEditAlarmSettingPage(request, alarmEntity);
 
-    @GetMapping("/settings/edit")
-    public String toEditAlarmSettingPage(HttpServletRequest request ,AlarmEntity alarmEntity){
-        request.setAttribute("alarmEntity", alarmEntity);
-        if (!alarmService.setAttributeBYMonitorAndPollution(request)){
-            return "500";
-        }
-        return Constants.EDITALARMSETTING;
-    }
-
-    @PostMapping("/settings/edit")
-    public String editAlarmSetting(HttpServletRequest request, AlarmEntity alarmEntity){
-        String msg = alarmService.editAlarmSetting(alarmEntity);
-        request.setAttribute(Constants.MSG, msg);
-        if (msg.contains("成功")) return "forward:/alarm/settings";
-        else return toEditAlarmSettingPage(request, alarmEntity);
-
-    }
+	}
 
 }
--- src/main/java/com/shark/aio/alarm/mapper/AlarmMapping.java
+++ src/main/java/com/shark/aio/alarm/mapper/AlarmMapping.java
@@ -8,45 +8,37 @@ import java.util.List;
 @Mapper
 public interface AlarmMapping {
 
-    @Select("select * from alarm_settings")
-    public List<AlarmEntity> getAllAlarmSettings();
+	@Select("select * from alarm_settings")
+	public List<AlarmEntity> getAllAlarmSettings();
 
-    @Select("select * from alarm_settings " +
-            "where monitor_class like #{feature} " +
-            "or monitor_value like #{feature} " +
-            "or lower_limit like #{feature} " +
-            "or upper_limit like #{feature} " +
-            "or message like #{feature}")
-    public List<AlarmEntity> getAlarmSettingsByFeatures(String feature);
+	@Select("select * from alarm_settings " + "where monitor_class like #{feature} "
+			+ "or monitor_value like #{feature} " + "or lower_limit like #{feature} "
+			+ "or upper_limit like #{feature} " + "or message like #{feature}")
+	public List<AlarmEntity> getAlarmSettingsByFeatures(String feature);
 
-    @Select("select monitor_value from alarm_settings where monitor_value=#{monitorValue}")
-    public String getMonitorValue(String monitorValue);
+	@Select("select monitor_value from alarm_settings where monitor_value=#{monitorValue}")
+	public String getMonitorValue(String monitorValue);
 
-    @Insert("insert into alarm_settings values (null,#{monitorClass},#{monitorValue},#{lowerLimit},#{upperLimit},#{message})")
-    public int insertAlarmEntity(AlarmEntity alarmEntity);
+	@Insert("insert into alarm_settings values (null,#{monitorClass},#{monitorValue},#{lowerLimit},#{upperLimit},#{message})")
+	public int insertAlarmEntity(AlarmEntity alarmEntity);
 
-    @Delete("delete from alarm_settings where id=#{id}")
-    public int deleteAlarmSettingById(int id);
+	@Delete("delete from alarm_settings where id=#{id}")
+	public int deleteAlarmSettingById(int id);
 
-    @Update("UPDATE `alarm_settings` " +
-            "SET " +
-            "`monitor_class` = #{monitorClass}, " +
-            "`monitor_value` = #{monitorValue}, " +
-            "`lower_limit` = #{lowerLimit}, " +
-            "`upper_limit` = #{upperLimit}, " +
-            "`message` = #{message} " +
-            "WHERE `id`=#{id}")
-    public int updateAlarmSetting(AlarmEntity alarmEntity);
+	@Update("UPDATE `alarm_settings` " + "SET " + "`monitor_class` = #{monitorClass}, "
+			+ "`monitor_value` = #{monitorValue}, " + "`lower_limit` = #{lowerLimit}, "
+			+ "`upper_limit` = #{upperLimit}, " + "`message` = #{message} " + "WHERE `id`=#{id}")
+	public int updateAlarmSetting(AlarmEntity alarmEntity);
 
-    @Select("select name from monitor_class")
-    public List<String> getAllMonitorClass();
+	@Select("select name from monitor_class")
+	public List<String> getAllMonitorClass();
 
-    @Insert("INSERT INTO `monitor_class` SET `name`=#{name}")
-    public int insertMonitorClass(String name);
+	@Insert("INSERT INTO `monitor_class` SET `name`=#{name}")
+	public int insertMonitorClass(String name);
 
-    @Select("select name from pollution")
-    public List<String> getAllPollutionName();
+	@Select("select name from pollution")
+	public List<String> getAllPollutionName();
 
-    @Insert("INSERT INTO `pollution` SET `name`=#{name}")
-    public int insertPollution(String name);
+	@Insert("INSERT INTO `pollution` SET `name`=#{name}")
+	public int insertPollution(String name);
 }
